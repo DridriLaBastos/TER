7205a8f3dbc95e84ca635aa91fd935903c26fe3f
DridriLaBastos <DridriLaBastos@users.noreply.github.com>
2020-04-06 13:43:10 +0200

    WEIGHTS_SIZE = 3

--- weight.hpp
+++ weight.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <iostream>
 
-constexpr size_t WEIGHTS_SIZE = 1;
+constexpr size_t WEIGHTS_SIZE = 3;
 
 //TODO: peut être passer ça en std::vector ?
 using Weight = std::array<int, WEIGHTS_SIZE>;
